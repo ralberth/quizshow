640ef401a48eb4c48743e66f494ce0aad4315515
Rich Alberth <ralberth@amazon.com>
2019-08-07 17:07:26 -0400

    1st graphql schema, ddb table quiz

--- server/bin/server.ts
+++ server/bin/server.ts
@@ -4,4 +4,9 @@ import cdk = require('@aws-cdk/core');
 import { QuizShowStack } from '../lib/QuizShowStack';
 
 const app = new cdk.App();
-new QuizShowStack(app, 'QuizShow');
+new QuizShowStack(app, 'QuizShow', {
+    env: {
+        region: process.env['AWS_REGION'],
+        account: process.env['AWS_ACCOUNT']
+    }
+});
--- server/lib/QuizShowStack.ts
+++ server/lib/QuizShowStack.ts
@@ -1,9 +1,14 @@
 import cdk = require('@aws-cdk/core');
 import iam = require('@aws-cdk/aws-iam');
+import appsync = require('@aws-cdk/aws-appsync');
 import cognito = require('@aws-cdk/aws-cognito');
+import dynamo = require('@aws-cdk/aws-dynamodb');
 import { AuthFlow } from '@aws-cdk/aws-cognito';
 import { ServicePrincipal } from '@aws-cdk/aws-iam';
 import { Duration } from '@aws-cdk/core';
+import { AttributeType, ProjectionType } from '@aws-cdk/aws-dynamodb';
+var fs = require('fs');
+var yaml = require('js-yaml');
 
 export class QuizShowStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -26,5 +31,60 @@ export class QuizShowStack extends cdk.Stack {
             userPool: userPool,
             enabledAuthFlows: [ AuthFlow.USER_PASSWORD ]
         });
-    }
+
+        const mySync = new appsync.CfnGraphQLApi(this, "QuizShow", {
+            name: 'QuizShow',
+            authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+            userPoolConfig: {
+                awsRegion: 'us-east-1',
+                defaultAction: 'ALLOW',
+                userPoolId: userPool.userPoolId
+            }
+        });
+
+        const graphql = new appsync.CfnGraphQLSchema(this, 'QuizShowSchema', {
+            apiId: mySync.attrApiId,
+            definition: fs.readFileSync('src/schema.gql', 'utf8')
+        });
+
+        const ddbQuiz = new dynamo.Table(this, 'quiz', {
+            tableName: 'quiz',
+            partitionKey: { name: 'id', type: AttributeType.NUMBER },
+            readCapacity: 5,
+            writeCapacity: 5
+        });
+        ddbQuiz.addGlobalSecondaryIndex({
+            indexName: 'userQuiz',
+            partitionKey: { name: 'owner', type: AttributeType.STRING },
+            sortKey: { name: 'id', type: AttributeType.NUMBER },
+            projectionType: ProjectionType.INCLUDE,
+            nonKeyAttributes: [ 'title' ]
+      });
+
+      const quizDataSource = new appsync.CfnDataSource(this, 'quiz_table', {
+          apiId: mySync.attrApiId,
+          name: 'quiz',
+          serviceRoleArn: role.roleArn,
+          type: 'AMAZON_DYNAMODB',
+          dynamoDbConfig: {
+            awsRegion: 'us-east-1',
+            tableName: 'quiz'
+          }
+      });
+      
+      const resolvers = yaml.safeLoad(fs.readFileSync('src/resolvers.yaml', 'utf8'));
+      resolvers.forEach((resolver:any)=> {
+          const r = new appsync.CfnResolver(this, `${resolver.type}_${resolver.field}`, {
+             apiId: mySync.attrApiId,
+             kind: 'UNIT',
+             typeName: resolver.type,
+             fieldName: resolver.field,
+             dataSourceName: resolver.dataSource,
+             requestMappingTemplate: resolver.requestMapping,
+             responseMappingTemplate: resolver.responseMapping
+          });
+          r.addDependsOn(quizDataSource);
+          r.addDependsOn(graphql)
+      });
+  }
 }
