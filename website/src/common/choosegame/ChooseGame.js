d3e5397f810ea458bbcdac67fda7ad77f4382f0d
tyler-austin <tyler.e.austin@gmail.com>
2019-09-13 12:36:02 -0400

    ChooseGame and children PaperGame now use Link over redirect which adds location to browser history for back/forward buttons

--- website/src/common/choosegame/ChooseGame.js
+++ website/src/common/choosegame/ChooseGame.js
@@ -1,14 +1,13 @@
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import React, { useState } from 'react';
-import { Redirect } from 'react-router';
+import { makeStyles } from '@material-ui/core/styles';
 import { useAppSyncQuery } from "../../graphql/useAppSyncHooks";
 import Loading from '../Loading';
 import PaperGame from "./PaperGame";
 import { ALL_GAMES_GQL } from "../../graphql/graphqlQueries";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   title: {
     fontWeight: `bold`,
     margin: `2rem 0`,
@@ -18,29 +17,20 @@ const useStyles = makeStyles(theme => ({
 
 const ChooseGame = ({ uriPrefix }) => {
   const classes = useStyles();
-  const [redirectToGame, setRedirectToGame] = useState(null);
   const { loading, data } = useAppSyncQuery(ALL_GAMES_GQL);
-
-  const setupGame = gameId => setRedirectToGame(`${uriPrefix}/${gameId}`);
 
   if (loading)
     return <Loading />;
-
-  if (redirectToGame)
-    return (<Redirect to={redirectToGame} />);
 
   return (
     <div className={classes.root} >
       <Typography className={classes.title} variant="h5">Choose A Game</Typography>
       <Grid container justify="center" alignItems="center">
-        {
-          data.map(
-            game => <PaperGame key={game.gameId} {...game} playCallback={setupGame}/>
-          )
-        }
+        { data.map(({gameId, title}) => <PaperGame key={gameId} {...{gameId, uriPrefix, title}}/>) }
       </Grid>
     </div>
   );
+
 }
 
 export default ChooseGame;
--- website/src/common/choosegame/PaperGame.js
+++ website/src/common/choosegame/PaperGame.js
@@ -6,48 +6,50 @@ import Typography from '@material-ui/core/Typography';
 import { grey } from '@material-ui/core/colors';
 import Fab from '@material-ui/core/Fab';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
-  paper: {
-    width: '100%',
-    maxWidth: 400,
-    margin: theme.spacing(1),
-    cursor: `pointer`,
-  },
   fab: {
     margin: theme.spacing(2),
   },
   gameName: {
     color: grey[800],
-    fontWeight: `bold`,
+    fontWeight: `900`,
     textAlign: `center`,
+    textDecoration: `none`,
   },
+  paperLink: {
+    width: '100%',
+    maxWidth: 400,
+    margin: theme.spacing(1),
+    cursor: `pointer`,
+    textTransform: `uppercase`,
+    textDecoration: `none`,
+    "&:focus, &:hover, &:visited, &:link, &:active": {
+        textDecoration: `none`,
+    },
+  }
 }));
 
-const PaperGame = ({ gameId, title, playCallback }) => {
+const PaperGame = ({ gameId, uriPrefix, title }) => {
   const classes = useStyles();
-
-  const playClicked = () => {
-    if (gameId) {
-      playCallback(gameId);
-    }
-  }
+  const path = `${uriPrefix}/${gameId}`;
 
   return (
-      <Paper className={classes.paper} onClick={playClicked} >
-        <Grid container direction="row" justify="center" alignItems="center">
-          <Grid item xs>
-            <Typography variant="h6" className={classes.gameName}>
-              {title}
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Fab size="small" color="primary" aria-label="add" className={classes.fab}>
-              <PlayArrowIcon onClick={playClicked} />
-            </Fab>
-          </Grid>
+    <Paper component={Link} to={path} className={classes.paperLink} >
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs>
+          <Typography variant="subtitle1" className={classes.gameName}>
+            { title }
+          </Typography>
         </Grid>
-      </Paper>
+        <Grid item>
+          <Fab size="small" color="primary" aria-label="add" className={classes.fab}>
+            <PlayArrowIcon />
+          </Fab>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
 
